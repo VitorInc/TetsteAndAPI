2d86de40e2bbd4c67f2600eb45c9bf83dc97b213
VitorInc <vitordc.contato@gmail.com>
2021-06-30 07:33:34 -0300

    Project start

---
+++ APIrestID/src/main/java/com/example/APIrestID/ApIrestIdApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIrestID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIrestIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIrestIdApplication.class, args);
+	}
+
+}
---
+++ APIrestID/src/test/java/com/example/APIrestID/ApIrestIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIrestID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIrestIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
