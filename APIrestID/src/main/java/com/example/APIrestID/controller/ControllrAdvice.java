ed3a49faf3638125519309167d2722b4de4d5f4f
VitorInc <vitordc.contato@gmail.com>
2021-07-01 09:39:13 -0300

    forgot the advice

--- APIrestID/src/main/java/com/example/APIrestID/controller/ControllrAdvice.java
+++ APIrestID/src/main/java/com/example/APIrestID/controller/ControllrAdvice.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class ControllrAdvice {
     final static Logger LOGGER = LoggerFactory.getLogger(ControllrAdvice.class);
 
